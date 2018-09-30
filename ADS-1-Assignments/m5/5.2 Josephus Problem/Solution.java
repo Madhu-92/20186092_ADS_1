54bbb86014fd364f9bdf9f73a345f4c303dbe9c0
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-30 22:04:19 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

---
+++ ADS-1-Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,149 @@
+import java.util.Scanner;
+/**
+ * Queue class.
+ */
+class Queue {
+    /**
+     * size variable.
+     */
+    private int size;
+    /**
+     * Node obj creation.
+     */
+    private Node queue;
+    /**
+     * Head node creation for referance.
+     */
+    private Node head;
+    /**
+     * Node class.
+     */
+    class Node {
+        /**
+         * data variable to store the data.
+         */
+        private int data;
+        /**
+         * Node object to point to the next Node.
+         */
+        private Node link;
+        /**
+         * Node constructor without arguments.
+         */
+        Node() {
+            this.data = 0;
+            this.link = null;
+        }
+        /**
+         * Node constructor with arguments.
+         *
+         * @param      nodeData  The data
+         */
+        Node(final int nodeData) {
+            this.data = nodeData;
+            this.link = null;
+        }
+    }
+    /**
+     * Queue constructor.
+     */
+    Queue() {
+        queue = new Node();
+        size = 0;
+    }
+    /**
+     * enQueue method to add the element at the head.
+     *
+     * @param      data  The data
+     */
+    public void enQueue(final int data) {
+        if (size == 0) {
+            head = new Node(data);
+            size++;
+            return;
+        }
+        Node start = new Node(data);
+        start.link = head;
+        head = start;
+        size++;
+        return;
+    }
+    /**
+     * method to remove the element at the end.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int deQueue() {
+        Node current = head;
+        if (size == 1) {
+            size--;
+            int n = head.data;
+            head = null;
+            return current.data;
+        }
+
+        int counter = 1;
+        while (counter < size - 1) {
+            counter++;
+            current = current.link;
+        }
+        int data = current.link.data;
+        current.link = null;
+        size--;
+        return data;
+    }
+    /**
+     * Checks weather Queue is empty or not.
+     *
+     * @return     True if empty else False.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    /**
+     * Getter method for sizes.
+     *
+     * @return     The size.
+     */
+    public int getSize() {
+        return size;
+    }
+}
+/**
+ * Solution class.
+ */
+public final class Solution {
+    /**
+     * Sollution constructor.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Queue qObj = new Queue();
+        Scanner scan = new Scanner(System.in);
+        int input = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < input; i++) {
+            String[] array = scan.nextLine().split(" ");
+            int num = Integer.parseInt(array[0]);
+            int pos = Integer.parseInt(array[1]);
+            for (int j = 0; j < num; j++) {
+                qObj.enQueue(j);
+            }
+            while (!qObj.isEmpty() && num != 1) {
+                for (int k = 1; k < pos; k++) {
+                    qObj.enQueue(qObj.deQueue());
+                }
+                System.out.print(qObj.deQueue() + " ");
+                num--;
+            }
+            System.out.print(qObj.deQueue());
+            System.out.println();
+        }
+    }
+}
