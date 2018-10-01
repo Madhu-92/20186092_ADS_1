68fe273e1d067e710d86fba5fa24dbb75927dd59
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-01 19:33:25 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1-Assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-Assignments/m7/7.1 Steque/Solution.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public final class Solution {
+    /**
+     * Solution constructor.
+     */
     private Solution() {
-
+        /**
+         * Empty Constructor
+         */
     }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         Steque obj = new Steque();
