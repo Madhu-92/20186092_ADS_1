a0bf77e7d60e891cf12f637165b7459439283a1e
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-16 19:33:28 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1-Assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,51 +1,105 @@
 import java.util.Scanner;
+/**
+ * Class for median finding.
+ */
 class MedianFinding {
-    
+    /**
+     * creating an object for MinPQ class.
+     */
     private MinPQ<Double> minObj;
+    /**
+     * creating an object for MaxPQ class.
+     */
     private MaxPQ<Double> maxObj;
-    
+    /**
+     * Constructs the object.
+     */
     MedianFinding() {
         minObj = new MinPQ<Double>();
         maxObj = new MaxPQ<Double>();
     }
-    
+    /**
+     * this method inserts element into minheap.
+     *
+     * @param      item  The item
+     */
     public void minInsert(final Double item) {
         minObj.insert(item);
     }
-    
+    /**
+     * this method inserts element into maxheap.
+     *
+     * @param      item  The item
+     */
     public void maxInsert(final Double item) {
         maxObj.insert(item);
     }
-    
+    /**
+     * Gets the minimum element from minheap.
+     *
+     * @return     The minimum element from minheap.
+     */
     public Double getMin() {
         return minObj.min();
     }
-    
+    /**
+     * Gets the maximum element from maxheap.
+     *
+     * @return     The maximum element from maxheap.
+     */
     public Double getMax() {
         return maxObj.max();
     }
-    
+    /**
+     * this method deletes the min element from minheap
+     *
+     * @return     returns the deleted element from minheap
+     */
     public Double delMin() {
         return minObj.delMin();
     }
-    
+    /**
+     * this method deletes the max element from maxheap
+     *
+     * @return     returns the deleted element from maxheap
+     */
     public Double delMax() {
         return maxObj.delMax();
     }
-    
+    /**
+     * Gets the size of maxheap.
+     *
+     * @return     the size of maxheap.
+     */
     public int getMaxSize() {
         return maxObj.size();
     }
-
+    /**
+     * the size of minheap
+     *
+     * @return     the size of minheap.
+     */
     public int getMinSize() {
         return minObj.size();
     }
 }
-
+/**
+ * solution class.
+ */
 final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //Blank constructor.
     }
+    /**
+     * main method is used to perform the operations
+     * Time complexity for this method is O(N).
+     * N is the number of inputs.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         MedianFinding medianObj = new MedianFinding();
