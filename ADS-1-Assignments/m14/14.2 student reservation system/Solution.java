efa22fbc991d4c93b60108c99427d426ee1cb0f8
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-17 19:54:31 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-Assignments/m14/14.2 student reservation system/Solution.java
+++ ADS-1-Assignments/m14/14.2 student reservation system/Solution.java
@@ -1,66 +1,125 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Comparator;
-
+/**
+ * Class for students.
+ */
 class Students {
-    
+    /**
+     * student info object array.
+     */
     private StudentInfo[] info;
-    
+    /**
+     * num variable.
+     */
     private int num;
-    
+    /**
+     * size variable.
+     */
     private int size;
-    
+    /**
+     * Constructs the object.
+     *
+     * @param      data  The data
+     */
     Students(final int data) {
         num = data;
         info = new StudentInfo[num];
         size = 0;
     }
-    
+    /**
+     * Time complexity is O(1)
+     * add's the element at last position of array.
+     *
+     * @param      stud  The stud
+     */
     public void add(final StudentInfo stud) {
         if (size == info.length) {
             resize();
         }
         info[size++] = stud;
     }
-
+    /**
+     * Resizes the array.
+     */
     public void resize() {
         info = Arrays.copyOf(info, info.length * 2);
     }
-
+    /**
+     * Gets the information.
+     *
+     * @return     The information.
+     */
     public StudentInfo[] getInfo() {
         return info;
     }
 }
 
-
+/**
+ * Class for category criteria.
+ */
 class CategoryCriteria {
-    
+    /**
+     * Constructs the object.
+     */
     private CategoryCriteria() {
         //Blank constructor.
     }
-    
+    /**
+     * Insertion class object.
+     */
     private Insertion mergee = new Insertion();
-    
+    /**
+     * No of students applied.
+     */
     private int applied;
-    
+    /**
+     * No of vacancies.
+     */
     private int vacancies;
-    
+    /**
+     * No of ur vacancies.
+     */
     private int ur;
-    
+    /**
+     * No of bc vacancies.
+     */
     private int bc;
-    
+    /**
+     * No of sc vacancies.
+     */
     private int sc;
-    
+    /**
+     * No of st vacancies.
+     */
     private int st;
-    
+    /**
+     * Size of the array.
+     */
     private int asize;
-    
+    /**
+     * Duplicated array for sorted objects.
+     */
     private StudentInfo[] dupSorted;
-    
+    /**
+     * Size of array.
+     */
     private int size;
-    
+    /**
+     * Array to store objects according to category.
+     */
     private StudentInfo[] catSorted;
-    
+    /**
+     * Constructs the object.
+     *
+     * @param      app     The application
+     * @param      vac     The vac
+     * @param      urev    The urev
+     * @param      bv      BC vacancies
+     * @param      sv      SC vacancies
+     * @param      stv     The stv
+     * @param      sorted  The sorted
+     */
     CategoryCriteria(final int app, final int vac, final int urev,
                      final int bv, final int sv, final int stv,
                      final StudentInfo[] sorted) {
@@ -76,7 +135,15 @@ class CategoryCriteria {
         catSorted = new StudentInfo[vacancies];
 
     }
-
+    /**
+     * Checks whether the Object contained in Array.
+     * Time complexity is O(N)
+     * it checks the element from start to array to end.
+     *
+     * @param      student  The student
+     *
+     * @return     returns true else false
+     */
     private boolean contains(final StudentInfo student) {
         for (int i = 0; i < asize; i++) {
             if (catSorted[i].getName().equals(student.getName())) {
@@ -85,7 +152,12 @@ class CategoryCriteria {
         }
         return false;
     }
-    
+    /**
+     * Categorising According to Criteria.
+     * Time complexity is O(N)
+     * it will allot the seats in the resesrved category
+     * using seat vacancies.
+     */
     public void catCriteria() {
         for (int i = 0; i < ur; i++) {
             catSorted[asize++] = dupSorted[i];
@@ -124,14 +196,20 @@ class CategoryCriteria {
         mergee.print(catSorted);
     }
 }
-
-
-
+/**
+ * Solution class.
+ */
 public final class Solution {
-    
+    /**
+     * Constructor.
+     */
     private Solution() {
     }
-    
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int studcount = Integer.parseInt(sc.nextLine());
@@ -162,13 +240,25 @@ public final class Solution {
         ccobj.catCriteria();
     }
 }
-
+/**
+ * Class for insertion.
+ */
 class Insertion {
-    
+    /**
+     * Constructor.
+     */
     Insertion() {
         //Blank constructor.
     }
-
+    /**
+     * Worst Case Complexity is O(N^2).
+     * It Iterates through out the array two times due to nested for loop.
+     *
+     * @param      a           { parameter_description }
+     * @param      lo          The lower
+     * @param      hi          The higher
+     * @param      comparator  The comparator
+     */
     public void insertionSort(final StudentInfo[] a, final int lo,
                               final int hi, final Comparator comparator) {
         for (int i = lo; i <= hi; i++) {
@@ -177,20 +267,39 @@ class Insertion {
             }
         }
     }
-
+    /**
+     * Swapping Elements in the Array.
+     *
+     * @param      a     Student Item
+     * @param      i     index i
+     * @param      j     index j
+     */
     private  void exch(final StudentInfo[] a, final int i,
                        final int j) {
         StudentInfo swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
-
-
+    /**
+     * Less method.
+     *
+     * @param      a     Student Item 1
+     * @param      b     Student Item 2
+     * @param      comp  The component
+     *
+     * @return     Returns boolean
+     */
     private boolean less(final StudentInfo a, final StudentInfo b,
                          final Comparator comp) {
         return comp.compare(a, b) < 0;
     }
-    
+    /**
+     * to print the Output
+     * Time complexity is O(N)
+     * prints all the values in array.
+     *
+     * @param      items  The items
+     */
     public void print(final StudentInfo[] items) {
         for (int i = 0; i < items.length; i++) {
             System.out.println(items[i].getName() + ","
--- ADS-1-Assignments/m14/14.2 student reservation system/StudentInformation.java
+++ ADS-1-Assignments/m14/14.2 student reservation system/StudentInformation.java
@@ -1,16 +1,51 @@
 import java.util.Comparator;
-
+/**
+ * student information class.
+ */
 class StudentInfo {
-
+    /**
+     * name variable.
+     */
     private String name;
+    /**
+     * dateofbirth.
+     */
     private int[] dob;
+    /**
+     * sub1 variable.
+     */
     private int sub1;
+    /**
+     * sub2 variable.
+     */
     private int sub2;
+    /**
+     * sub3 variable.
+     */
     private int sub3;
+    /**
+     * total marks.
+     */
     private int totmarks;
+    /**
+     * category.
+     */
     private String category;
+    /**
+     * number of days.
+     */
     private static final int DAYS = 30;
-
+    /**
+     * Constructs the object.
+     *
+     * @param      n           name.
+     * @param      b           dob.
+     * @param      s1          The s 1
+     * @param      s2          The s 2
+     * @param      s3          The s 3
+     * @param      totalmarks  The totalmarks
+     * @param      cat         The cat
+     */
     StudentInfo(final String n, final String b, final int s1,
                 final int s2, final int s3, final int totalmarks,
                 final String cat) {
@@ -26,19 +61,33 @@ class StudentInfo {
         totmarks = totalmarks;
         category = cat;
     }
-
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
     public int getMarks() {
         return totmarks;
     }
-
+    /**
+     * Gets the category.
+     *
+     * @return     The category.
+     */
     public String getCategory() {
         return category;
     }
-
+    /**
+     * comparator.
+     */
     static Comparator<StudentInfo> comp = new Comparator<StudentInfo>() {
         public int compare(final StudentInfo s1, final StudentInfo s2) {
             if (s2.totmarks - s1.totmarks != 0) {
