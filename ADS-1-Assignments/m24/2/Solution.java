adf91bf64a11988eebe6448ac9ce852d1aaea038
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-27 13:15:29 +0530

    2

---
+++ ADS-1-Assignments/m24/2/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+final class Solution {
+        private Solution() {
+        //Blank constructor.
+    }
+    
+    public static void main(final String[] args) {
+        LinearProbingHashST<Integer, String> lpObj =
+            new LinearProbingHashST<Integer, String>();
+        Scanner sc = new Scanner(System.in);
+        int inputs = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < inputs; i++) {
+            String[] tokens = sc.nextLine().split(",");
+            String str = tokens[1] + "," + tokens[2];
+            lpObj.put(Integer.parseInt(tokens[0]), str);
+        }
+        int getValues = Integer.parseInt(sc.nextLine());
+        for (int j = 0; j < getValues; j++) {
+            String[] tokens = sc.nextLine().split(" ");
+            String str = lpObj.get(Integer.parseInt(tokens[1]));
+            if (str != null) {
+                String[] tokens2 = str.split(",");
+                if (Integer.parseInt(tokens[2]) == 1) {
+                    System.out.println(tokens2[0]);
+                } else {
+                    System.out.println(Double.parseDouble(tokens2[1]));
+                }
+            }
+        }
+    }
+
+}
