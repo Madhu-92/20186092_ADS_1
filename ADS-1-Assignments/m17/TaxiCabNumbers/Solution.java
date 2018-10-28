424c774c8a5a144e8eab6b251fdaa5632c60ec9b
Madhu-92 <madhu.ktej@msitprogram.net>
2018-10-28 13:30:55 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-Assignments/m17/TaxiCabNumbers/Solution.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+/**
+ * Class for cube sum.
+ */
+class CubeSum implements Comparable<CubeSum> {
+    /**
+     * sum variable.
+     */
+    private final int sum;
+    /**
+     * This variable is the first number.
+     */
+    private final int first;
+    /**
+     * this variable is the second number
+     */
+    private final int second;
+    /**
+     * Constructs the object.
+     *
+     * @param      first1     the first number
+     * @param      second1     the second number
+     */
+    CubeSum(final int first1, final int second1) {
+        this.sum = first1 * first1 * first1
+                   + second1 * second1 * second1;
+        this.first = first1;
+        this.second = second1;
+    }
+    /**
+     * Gets the sum.
+     *
+     * @return     The sum.
+     */
+    public int getSum() {
+        return this.sum;
+    }
+    /**
+     * Gets the first.
+     *
+     * @return     The first.
+     */
+    public int getFirst() {
+        return this.first;
+    }
+    /**
+     * Gets the second.
+     *
+     * @return     The second.
+     */
+    public int getSecond() {
+        return this.second;
+    }
+    /**
+     * This method compares both sums
+     *
+     * @param      that  The that
+     *
+     * @return     returns the integer value after
+     *             comparision
+     */
+    public int compareTo(final CubeSum that) {
+        if (this.sum < that.sum) {
+            return -1;
+        }
+        if (this.sum > that.sum) {
+            return +1;
+        }
+
+        return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return sum + " = " + first + "^3" + " + " + second + "^3";
+    }
+
+
+
+}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * input numbers limit
+     */
+    static final int INPUT = 600;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main method to print the Nth number of M times of combinitions
+     * Time complexity of method is O(2N)
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] tokens = sc.nextLine().split(" ");
+        int count = 0;
+        int number = Integer.parseInt(tokens[0]);
+        int times = Integer.parseInt(tokens[1]);
+        int temp = -1;
+
+        MinPQ<CubeSum> minpq = new MinPQ<CubeSum>();
+        for (int i = 0; i <= INPUT; i++) {
+            minpq.insert(new CubeSum(i, i));
+        }
+
+        while (!minpq.isEmpty()) {
+            CubeSum s = minpq.delMin();
+            if (temp == s.getSum()) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count == times - 1) {
+                number--;
+                if (number == 0) {
+                    System.out.println(s.getSum());
+                    break;
+                }
+            }
+
+            temp = s.getSum();
+            if (s.getSecond() < INPUT) {
+                minpq.insert(new CubeSum(
+                              s.getFirst(), s.getSecond() + 1));
+            }
+        }
+    }
+
+}
